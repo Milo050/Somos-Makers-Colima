c33dc950703101ddadb45d11f1720afe58adcb09
Milo050 <pablomilo25@gmail.com>
2025-04-18 17:36:38 -0600

    Create Tienda.js

---
+++ src/Tienda.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tienda = () => {
+  return (
+    <div>
+      <h1>Bienvenido a Somos Makers Colima</h1>
+      <p>Aquí irá el catálogo de productos impresos en 3D.</p>
+    </div>
+  );
+};
+
+export default Tienda;
